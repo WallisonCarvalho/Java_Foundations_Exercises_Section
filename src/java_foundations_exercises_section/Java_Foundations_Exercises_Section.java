757eb96104078393a837790680b61b1ea9b7ee82
Wallison Carvalho <wallison.ecarvalho95@gmail.com>
2022-11-12 12:03:47 -0300

    Create Java Project

---
+++ src/java_foundations_exercises_section/Java_Foundations_Exercises_Section.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_foundations_exercises_section;
+
+/**
+ *
+ * @author walli
+ */
+public class Java_Foundations_Exercises_Section {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello World ");
+    }
+    
+}
